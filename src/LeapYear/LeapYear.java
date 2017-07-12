9afbc0802d048847ef577eae770c7b69966d3b9c
MasterWYJP <wesleyyjpark@gmail.com>
2017-07-12 12:21:04 -0500

    Leap Year

---
+++ src/LeapYear/LeapYear.java
@@ -0,0 +1,32 @@
+package LeapYear;
+
+import javax.swing.*;
+
+/**
+ * Created by student on 7/12/2017.
+ */
+public class LeapYear {
+    public static void main(String args[]){
+
+        //asks user what year it is
+        String input = JOptionPane.showInputDialog(null,
+                "What year is it?",
+                "Leap Year",
+                JOptionPane.QUESTION_MESSAGE);
+        //converts the input to an integer
+        int year = Integer.parseInt(input);
+
+        //Sees if the year can be divided by 4 and not by 100, if not, goes to the or divided by 400
+        if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0)) {
+            JOptionPane.showMessageDialog(null,
+                    year + " is a leap year");
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    year  +" is not a leap year");
+        }
+
+
+
+
+    }
+}
